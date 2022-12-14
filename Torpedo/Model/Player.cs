fa14d7ab2cf0a01c1781eb5ec210cd47330f7423
VassZsolt <rtmzsolt@gmail.com>
2022-12-14 11:39:04 +0100

    Player.cs: The Shoots are stored in this class.

--- Torpedo/Model/Player.cs
+++ Torpedo/Model/Player.cs
@@ -12,11 +12,13 @@ namespace Torpedo.Model
         private string _name;
         public Ship[] Ships;        
         public Board Board;
+        public List<Coordinate> Shoots;
 
         public Player(string name, int numberOfShips, int boardSize) {
             _name = name;
             Board = new Board(boardSize);
             Ships= new Ship[numberOfShips];
+            Shoots = new List<Coordinate>();
         }
     }
 }
