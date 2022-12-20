95c6954e1085b3231ba97b015c60b4426218ccfb
VassZsolt <rtmzsolt@gmail.com>
2022-12-20 12:23:12 +0100

    Modified Coordinate class getter and setter methods.

--- Torpedo/Model/Coordinate.cs
+++ Torpedo/Model/Coordinate.cs
@@ -5,18 +5,23 @@
     /// </summary>
     internal class Coordinate
     {
-        public Column Column { get; private set; } // A-J
-        public int Row { get; private set; }
+        private Column _column; // A-J
+        private int _row;
+
+        public void SetColumn(Column column) => _column = column;
+        public Column Column => _column;
+        public void SetRow(Column column) => _column = column;
+        public int Row => _row;
 
         public Coordinate(Column column, int row)
         {
-            Column = column;
-            Row = row;
+            _column = column;
+            _row = row;
         }
 
         public override string ToString()
         {
-            return $"{Column}{Row}";
+            return $"{Column}{_row}";
         }
     }
 }
