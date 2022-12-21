a68f00868cb9f0c3d97f089d23f68de5cf4e5263
Niki <mnikolett2001@gmail.com>
2022-12-21 15:46:23 +0100

    Merged GameController and InitialGame classes.

--- Torpedo/Controllers/GameController.cs
+++ Torpedo/Controllers/GameController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using NationalInstruments.Torpedo.Controllers;
+using System.Collections.Generic;
 using NationalInstruments.Torpedo.Model;
 
 namespace NationalInstruments.Torpedo.ViewModel
@@ -8,37 +10,60 @@ namespace NationalInstruments.Torpedo.ViewModel
     /// </summary>
     internal class GameController
     {
-        private Player _firstPlayer;
-        private Player _secondPlayer;
+        public Player Firstplayer;
+        public Player SecondPlayer;
         private Random _random = new Random();
-        private InitialGame _game;
 
-        public GameController(GameMode gameMode)
+        private const int _numberOfShips = 5;
+        private const int _sizeOfBoard = 10;
+
+        private Player _player1 = new Player(0, 0);
+        private Player _player2 = new Player(0, 0);
+        private GameMode _gameMode;
+        private ShipPlacementController _shipPlacementController = new ShipPlacementController(_sizeOfBoard);
+        public Match Match;
+
+        public GameController(GameMode gameMode, string playerOneName, string? playerTwoName)
         {
-            _game = new InitialGame(gameMode);
-            ChooseFirstPlayer();
-            PlayGame();
+            _gameMode = gameMode;
+            CreatePlayers(playerOneName, playerTwoName);
         }
-
-        private void ChooseFirstPlayer()
+        private void CreatePlayers(string playerOneName, string? playerTwoName)
         {
-            if (_random.Next(2) == 0)
+            _player1 = new Player(_numberOfShips, _sizeOfBoard);
+            _player1.Name = playerOneName;
+
+            if (_gameMode == GameMode.TwoPlayerMode)
             {
-                _firstPlayer = _game.Player1;
-                _secondPlayer = _game.Player2;
-                _game.Match.SetFirstPlayerName(_game.Player1.Name);
+                _player2 = new Player(_numberOfShips, _sizeOfBoard);
+                _player2.Name = playerTwoName;
             }
             else
             {
-                _firstPlayer = _game.Player2;
-                _secondPlayer = _game.Player1;
-                _game.Match.SetFirstPlayerName(_game.Player2.Name);
+                // TODO __Dede Zsolt__ Create an AI
+            }
+            Match = new Match(_player1, _player2);
+            ChooseFirstPlayer();
+        }
+        public void ChooseFirstPlayer()
+        {
+            if (_random.Next(2) == 0)
+            {
+                Firstplayer = _player1;
+                SecondPlayer = _player2;
+                Match.SetFirstPlayerName(_player1.Name);
+            }
+            else
+            {
+                Firstplayer = _player2;
+                SecondPlayer = _player1;
+                Match.SetFirstPlayerName(_player2.Name);
             }
         }
 
         private void PlayGame()
         {
-            Player actualPlayer = _firstPlayer;
+            Player actualPlayer = Firstplayer;
             while (!IsGameOver(actualPlayer))
             {
                 MakeShoot(actualPlayer);
@@ -48,13 +73,13 @@ namespace NationalInstruments.Torpedo.ViewModel
 
         private Player NextPlayer(Player player)
         {
-            if (player == _firstPlayer)
+            if (player == Firstplayer)
             {
-                return _secondPlayer;
+                return SecondPlayer;
             }
             else
             {
-                return _firstPlayer;
+                return Firstplayer;
             }
         }
         private void MakeShoot(Player actualPlayer)
@@ -82,7 +107,7 @@ namespace NationalInstruments.Torpedo.ViewModel
         private bool IsHit(Player player, Coordinate target)
         {
             Player enemy = NextPlayer(player);
-            for (int i = 0; i < enemy.Ships.Length; i++)
+            for (int i = 0; i < enemy.Ships.Count; i++)
             {
                 Ship ship = enemy.Ships[i];
                 if (ship.Status == false)
@@ -105,7 +130,7 @@ namespace NationalInstruments.Torpedo.ViewModel
 
         private bool IsGameOver(Player player)
         {
-            for (int i = 0; i < player.Ships.Length; i++)
+            for (int i = 0; i < player.Ships.Count; i++)
             {
                 if (player.Ships[i].Status == true)
                 {
@@ -113,6 +138,75 @@ namespace NationalInstruments.Torpedo.ViewModel
                 }
             }
             return true;
+        }
+        public void PlaceShip(int shipSize, Alignment align, Coordinate startPosition, Player player)
+        {
+            _shipPlacementController.SizeOfShip = shipSize;
+            _shipPlacementController.SetAlignment(align);
+            _shipPlacementController.SetStartPosition(startPosition);
+
+            if (_shipPlacementController.IsPossiblePlacement())
+            {
+                List<Coordinate> positions = _shipPlacementController.GenerateShipPositions;
+                Ship ship = new Ship();
+                ship.ShipSize = shipSize;
+                ship.ShipAlignment = align;
+                ship.StartPosition = startPosition;
+                foreach(Coordinate coordinate in positions)
+                {
+                    ship.Positions.Add(coordinate);
+                }
+                ship.Status = true;
+
+                player.Ships.Add(ship);
+            }
+        }
+
+        public Coordinate GetCoordinate(string buttonName)
+        {
+            Column column;
+            if (buttonName.Substring(0, 1) == "A")
+            {
+                column = Column.A;
+            }
+            else if (buttonName.Substring(0, 1) == "B")
+            {
+                column = Column.B;
+            }
+            else if (buttonName.Substring(0, 1) == "C")
+            {
+                column = Column.C;
+            }
+            else if (buttonName.Substring(0, 1) == "D")
+            {
+                column = Column.D;
+            }
+            else if (buttonName.Substring(0, 1) == "E")
+            {
+                column = Column.E;
+            }
+            else if (buttonName.Substring(0, 1) == "F")
+            {
+                column = Column.F;
+            }
+            else if (buttonName.Substring(0, 1) == "G")
+            {
+                column = Column.G;
+            }
+            else if (buttonName.Substring(0, 1) == "H")
+            {
+                column = Column.H;
+            }
+            else if (buttonName.Substring(0, 1) == "I")
+            {
+                column = Column.I;
+            }
+            else
+            {
+                column = Column.J;
+            }
+            string row = buttonName.Substring(1);
+            return new Coordinate(column, row: Convert.ToInt32(row));
         }
     }
 }
