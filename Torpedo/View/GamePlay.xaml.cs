90a96579b987f4be3314d9e05be0dc2ea2ca3a9c
Niki <mnikolett2001@gmail.com>
2022-12-22 15:06:39 +0100

    Made labels to show the data of the game

--- Torpedo/View/GamePlay.xaml.cs
+++ Torpedo/View/GamePlay.xaml.cs
@@ -66,7 +66,7 @@ namespace NationalInstruments.Torpedo.View
                     }
                 }
             }
-            
+
             Player actualPlayer = _controller.Firstplayer;
             while (!IsGameOver(actualPlayer))
             {
